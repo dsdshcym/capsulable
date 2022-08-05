44f515e7d8d75258efd8e262c1be5f7919a41fbe
Yiming Chen <dsdshcym@gmail.com>
2022-08-05 10:37:42 +0800

    delete unused Capsule.hello/0

--- lib/capsule.ex
+++ lib/capsule.ex
@@ -1,18 +1,2 @@
 defmodule Capsule do
-  @moduledoc """
-  Documentation for `Capsule`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Capsule.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/capsule_test.exs
+++ test/capsule_test.exs
@@ -1,8 +1,4 @@
 defmodule CapsuleTest do
   use ExUnit.Case
   doctest Capsule
-
-  test "greets the world" do
-    assert Capsule.hello() == :world
-  end
 end
