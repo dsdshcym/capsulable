7a6fe8b81d840e9447a319cdbe722b2437dabe66
Yiming Chen <dsdshcym@gmail.com>
2022-08-05 10:34:03 +0800

    mix new capsule

---
+++ lib/capsule.ex
@@ -0,0 +1,18 @@
+defmodule Capsule do
+  @moduledoc """
+  Documentation for `Capsule`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Capsule.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
