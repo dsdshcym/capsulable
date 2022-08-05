870c84a4683de2b27fdc24ff1736d1abbe7fa89f
Yiming Chen <dsdshcym@gmail.com>
2022-08-05 16:02:40 +0800

    handle Plug.Conn inside Capsule.Capsulable.Any
    - so that users of Capsule can provide their own implementation for Plug.Conn

--- lib/capsule/capsulable.ex
+++ lib/capsule/capsulable.ex
@@ -1,7 +1,28 @@
 defprotocol Capsule.Capsulable do
+  @fallback_to_any true
+
   @spec put(t(), any(), any()) :: t()
   def put(capsulable, key, value)
 
   @spec fetch(t(), any()) :: {:ok, any()} | :error
   def fetch(capsulable, key)
+end
+
+defimpl Capsule.Capsulable, for: Any do
+  def put(%Plug.Conn{} = conn, key, value) do
+    case conn.private[:__capsule__] do
+      nil ->
+        Plug.Conn.put_private(conn, :__capsule__, %{key => value})
+
+      capsule ->
+        Plug.Conn.put_private(conn, :__capsule__, Map.merge(capsule, %{key => value}))
+    end
+  end
+
+  def fetch(%Plug.Conn{} = conn, key) do
+    case conn.private[:__capsule__] do
+      %{^key => value} -> {:ok, value}
+      _ -> :error
+    end
+  end
 end
